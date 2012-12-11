0d2589485eb86394fa0af76b61e3d9f65a6a5de3
zhugexiaobo <zhugexiaobo@gmail.com>
2012-12-11 16:42:38 +0800

    Initial Commit

---
+++ tableView-timeLine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tableView-timeLine
+//
+//  Created by xiaobo.zhuge on 12-12-11.
+//  Copyright (c) 2012年 xiaobo.zhuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tableView-timeLine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tableView-timeLine
+//
+//  Created by xiaobo.zhuge on 12-12-11.
+//  Copyright (c) 2012年 xiaobo.zhuge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
